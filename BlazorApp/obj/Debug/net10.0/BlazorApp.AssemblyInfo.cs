19254aaf7f1b8be0a694678784d2a1470c127df0
Jamie Boulton <jb469@students.waikato.ac.nz>
2025-11-20 17:02:11 +1300

    Implemented parallel registers.

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7921d76de14f4e6ee6fc235051cf46571ee651c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fd3c087faeebdfdbf745f9c8e8658dfa953444")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
