6134e626cafbb743f121509a391076897095ab8e
Jamie Boulton <jb469@students.waikato.ac.nz>
2026-01-13 16:02:16 +1300

    Fixed live updating of generated Blockly code.

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5cda7a8b45aacbfccb5f1fbbf3c08cb321ce1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9657a8a16e2813ff370ef67ccd43028ab9d092db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
