b328f364f67eb48b7c60a5767d73aee6bdf65d29
Jamie Boulton <jb469@students.waikato.ac.nz>
2026-01-16 13:22:23 +1300

    Added the ability to show and hide the Code tab.
    Fixed an error that could be thrown if reloading a page that didn't have a Blockly component.

--- BlazorApp/Classes/UIComponents.cs
+++ BlazorApp/Classes/UIComponents.cs
@@ -1,5 +1,6 @@
 public class UIComponents {
     public bool WSIMInstructions;
+    public bool Code;
     public bool GPRegisters;
     public bool SPRegisters;
     public bool RAM;
@@ -13,9 +14,10 @@ public class UIComponents {
 
     public bool Blockly;
 
-    public UIComponents(bool WSIMInstructions = false, bool GPRegisters = false, bool SPRegisters = false, bool RAM = false, bool ParallelRegisters = false, bool SerialPort = false, bool LEDs = false, bool Switches = false, bool Buttons = false, bool SSDs = false, bool Library = false, bool Blockly = false)
+    public UIComponents(bool WSIMInstructions = false, bool Code = false, bool GPRegisters = false, bool SPRegisters = false, bool RAM = false, bool ParallelRegisters = false, bool SerialPort = false, bool LEDs = false, bool Switches = false, bool Buttons = false, bool SSDs = false, bool Library = false, bool Blockly = false)
     {
         this.WSIMInstructions = WSIMInstructions;
+        this.Code = Code;
         this.GPRegisters = GPRegisters;
         this.SPRegisters = SPRegisters;
         this.RAM = RAM;
